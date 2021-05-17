75d5af86e8a88d93b3142371f6b16593d00e663a
손창대 <changdae20@aiaracorp.com>
2021-05-17 11:30:28 +0900

    set timer function

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ class Vector{
         return new Vector(this.x * c, this.y * c);
     }
 }
+
 class Track{
 
     constructor(){
@@ -46,14 +47,30 @@ class Track{
         this.point.pop();
     }
     play(time_delta = 0.01){
+        // console.log("play");
+        // console.log(this);
         var dist_delta = bpm * time_delta / 60;
         var goal_vec = this.point[this.current_goal].add(this.current_point.mult(-1));
         goal_vec = goal_vec.normalize();
         this.current_point = this.current_point.add(goal_vec.mult(dist_delta));
-        if(this.current_point.add(this.point[this.current_goal].mult(-1)).get_norm()<=0.05){
+        if(this.current_point.add(this.point[this.current_goal].mult(-1)).get_norm()<=0.02){
             console.log("goal finish");
-            this.audio.play();
+            //this.audio.play();
             this.current_goal = (this.current_goal+1) % this.point.length;
         }
     }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("document ready");
+    var track = new Track();
+    track.add(new Vector(0,0));
+    track.add(new Vector(1,0));
+    bpm = 120;
+
+    console.log("track ready");
+    console.log(track);
+    var playtrack = setInterval(function(){
+        track.play()
+    },10);
+});
